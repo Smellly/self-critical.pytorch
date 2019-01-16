9b55e4fd9a62451ffbc9074304302e5faae1ef2b
jaycshen <schenxmu@gmail.com>
2019-01-16 05:53:53 -0800

    why

--- scripts/prepro_feats.py
+++ scripts/prepro_feats.py
@@ -46,6 +46,9 @@ preprocess = trn.Compose([
         trn.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
 ])
 
+import sys
+print(os.getcwd())
+print(sys.path)
 from misc.resnet_utils import myResnet
 import misc.resnet as resnet
 
